8f84545e969f36251ccb2ea4182afc83f50313c6
Melodie Bernard <mbernard@z1r10p3.42lyon.fr>
2023-11-20 17:17:42 +0100

    Libft with atoi and calloc corrected

---
+++ ft_atoi.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbernard <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/06 16:29:50 by mbernard          #+#    #+#             */
+/*   Updated: 2023/11/20 17:08:52 by mbernard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_is_space(char c)
+{
+	if (c == ' ' || c == '\f' || c == '\n')
+		return (1);
+	if (c == '\r' || c == '\t' || c == '\v')
+		return (1);
+	return (0);
+}
+
+int	ft_atoi(const char *str)
+{
+	int			i;
+	int			sign;
+	long long	nb;
+
+	i = 0;
+	sign = 1;
+	nb = 0;
+	while (ft_is_space(str[i]))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (ft_isdigit(str[i]))
+	{
+		if ((nb * 10 + (str[i] - '0')) < nb)
+			return (-(sign == 1));
+		nb = nb * 10 + (str[i] - '0');
+		i++;
+	}
+	return (((int)nb * sign));
+}
